d320ed675787d553f88f154837037ad25fcd0fee
Satoshi Suzuki <suzuki@precise.(none)>
2012-07-02 20:42:40 +0900

    ネストが深いのを浅くした

--- explain2.pl
+++ explain2.pl
@@ -13,61 +13,49 @@ my $mp = Data::MessagePack->new();
 my $db_user     = "root";
 my $db_passwd   = "";
 my $db_opt      = { RaiseError => 0, PrintError => 1 };
-my $handler     = DBIx::Handler->new("DBI:mysql:database=mysql:127.0.0.1", $db_user, $db_passwd, $db_opt);
+my $handler     = DBIx::Handler->new("DBI:mysql:database=information_schema:127.0.0.1", $db_user, $db_passwd, $db_opt);
+
+my $db;
+my $long_query_time = $handler->dbh->selectrow_array("select VARIABLE_VALUE from GLOBAL_VARIABLES where VARIABLE_NAME = 'LONG_QUERY_TIME'");
+$handler->dbh->do("SELECT SLEEP($long_query_time)");
 
 while(my $json = <STDIN>){
-
   # STDINがJSONじゃなかったら何もしない
   my $decode = eval { decode_json($json); };
-  unless ($@) {
+  next if ($@);
 
-    # STDINで受け取ったJSONに"sql"があり、さらにSELECT句が含まれていなければ、
-    # 受け取ったJSONをそのままMessagePackにして放出
-    if (defined $decode->{sql}) {
-      my ($select) = ($decode->{sql} =~ /(select[^\;]+)/i);
-
-      # もっとイケてる書き方がきっとある・・・
-      my ($db) = ($decode->{sql} =~ /^use ([^\;]+)/i) if ($decode->{sql} =~ /^use [^\;]+/i);
-
-      if (defined $select){
-        my $result;
-        my %data = %$decode;
-
-        # td-agent起動直後に$dbが未定義になってしまうことの対策
-        # 未定義だった場合は、全データベースでexplainしてみて、結果が返ってきた時のDB名を$dbにセット
-        # ただ、コレだと異なるデータベースに同名のテーブルがあったときにどうしようという感じ・・・
-        # (シャーディングされたのをがっちゃんこした場合とかはスキーマも同じになるから厄介)
-        if (defined $db) {
-          $handler->dbh->do("use $db");
-          $result = eval { $handler->dbh->selectall_arrayref( "explain $select", +{ Slice => {} } ); };
-        }else{
-          my $databases = $handler->dbh->selectcol_arrayref("show databases");
-          foreach (@$databases) {
-            $handler->dbh->do("use $_");
-            my $result = eval { $handler->dbh->selectall_arrayref( "explain $select", +{ Slice => {} } ); };
-            if (defined $result) {
-              $db = $_; 
-              last;
-            }   
-          }
-        }
-
-        # explain結果が$resultに入ってないとdieしてwhile抜けちゃうので定義済みの場合のみ
-        if (defined $result){
-          for (my $i=0;$i < (@$result);$i++){
-            my $key = "explain$i";
-            my %explains;
-            %explains = (%explains, ($key  => $result->[$i]));
-            %data = (%data, %explains);
-          }
-        }
-
-        # どのDBへのEXPLAINかどうかわかるように。
-        %data = (%data, (database => $db));
-        print $mp->pack(\%data);
-      }else{
-        print $mp->pack($decode);
-      }
-    }
+  # decodeしたJSONに"sql"キーがあり、そこにSELECT文が含まれていなければ、
+  # そのままMessagePackで放出する
+  my $select;
+  if (defined $decode->{sql} && $decode->{sql} =~ /(select[^\;]+)/i){
+    $select = $1;
+  }else{
+    print $mp->pack($decode);
+    next;
   }
+
+  # "use database"があれば、データベースを切り替える
+  if ($decode->{sql} =~ /^use ([^\;]+)/i){
+    $db = $1;
+    $handler->dbh->do("use $db");
+  }
+
+  unless (defined $db){
+    print $mp->pack($decode);
+    next;
+  }
+
+  my $data   = $decode;
+  my $result = eval { $handler->dbh->selectall_arrayref( "explain $select", +{Slice => {}});};
+
+  # explain結果が$resultに入ってないとdieしてwhile抜けちゃうので定義済みの場合のみ
+  if (defined $result){
+    for (my $i=0;$i < (@$result);$i++){
+      $data->{"explain$i"} = $result->[$i];
+    }
+    # どのDBへのEXPLAINかどうかわかるように。
+    $data->{database} = $db;
+  }
+
+  print $mp->pack($data);
 }
