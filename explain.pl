366e713a68eaba84d2f8d5e5e7e0ce1fe43e4df2
Satoshi Suzuki <suzuki@precise.(none)>
2012-07-31 12:37:39 +0900

    bugfix

--- explain.pl
+++ explain.pl
@@ -11,43 +11,42 @@ $| = 1;
 
 my $mp = Data::MessagePack->new();
 
-my $db_user   = "root";
-my $db_passwd = "";
+my $db_user   = "td-agent";
+my $db_passwd = "td-agent";
 my $db_opt    = { RaiseError => 0, PrintError => 1 };
 my $handler   = DBIx::Handler->new( "DBI:mysql:database=information_schema:127.0.0.1", $db_user, $db_passwd, $db_opt );
 
 my $db;
 my $long_query_time = $handler->dbh->selectrow_array(
-  "select VARIABLE_VALUE from GLOBAL_VARIABLES where VARIABLE_NAME = 'LONG_QUERY_TIME'");
+  "SELECT VARIABLE_VALUE FROM GLOBAL_VARIABLES WHERE VARIABLE_NAME = 'LONG_QUERY_TIME'");
 $handler->dbh->do("SELECT SLEEP($long_query_time)");
 
 while ( my $json = <STDIN> ) {
-  my $decode = eval { decode_json($json); };
+  my $slowlog = eval { decode_json($json); };
   next if ($@);
 
-  unless ( defined $decode->{sql} ) {
-    print $mp->pack($decode);
+  unless ( defined $slowlog->{sql} ) {
+    print $mp->pack($slowlog);
     next;
   }
 
-  my $select;
-  if ( $decode->{sql} =~ /(select[^\;]+)/i ) {
-    $select = $1;
+  my $select_statement;
+  if ( $slowlog->{sql} =~ /(select[^\;]+)/i ) {
+    $select_statement = $1;
   }
 
-  if ( $decode->{sql} =~ /^use ([^\;]+)/i ) {
-    $db = $1;
+  if ( $slowlog->{sql} =~ /^use ([^\;]+)/i ) {
+    $db = '`' . $1 . '`';
   }
 
-  if ( defined $db && defined $select ) {
+  if ( defined $db && defined $select_statement ) {
     $handler->dbh->do("use $db");
-    my $explains = eval { $handler->dbh->selectall_arrayref( "explain $select", +{ Slice => {} } ); };
-
-    unless ( $@ || @$explains == 0 ) {
-      $decode->{explain}  = $explains;
-      $decode->{database} = $db;
+    my $explains = eval { $handler->dbh->selectall_arrayref( "EXPLAIN $select_statement", +{ Slice => {} } ); };
+    if ( ref $explains eq 'ARRAY' ) {
+      $slowlog->{explain}  = $explains;
+      $slowlog->{database} = $db;
     }
   }
 
-  print $mp->pack($decode);
+  print $mp->pack($slowlog);
 }
